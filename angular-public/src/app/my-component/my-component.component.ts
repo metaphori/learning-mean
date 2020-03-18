8950d58ef569d3f8affe98b51f3449d0ca17eea3
Roberto Casadei <robyonrails@gmail.com>
2020-03-18 01:13:36 +0100

    More with custom component

--- angular-public/src/app/my-component/my-component.component.ts
+++ angular-public/src/app/my-component/my-component.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+export class MyComponentData {
+  title: string;
+  content: string;
+  elements: string[];
+  something: number;
+}
+
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html',
@@ -8,6 +15,15 @@ import { Component, OnInit } from '@angular/core';
 export class MyComponentComponent implements OnInit {
 
   constructor() { }
+
+  // title = 'my-component'
+  // content = 'my-component works???'
+
+  data: MyComponentData = {
+    title: 'my-component',
+    content: 'my-component works!!!',
+    elements: ['elem1','elem2']
+  };
 
   ngOnInit(): void {
   }
