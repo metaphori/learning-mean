99c176151a2fc7643124b791665a4991d77e23cc
Roberto Casadei <robyonrails@gmail.com>
2020-03-18 01:13:52 +0100

    Custom pipe myp: ng generate pipe myp

--- angular-public/src/app/app.module.ts
+++ angular-public/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { MypPipe } from './myp.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    MyComponentComponent,
+    MypPipe
   ],
   imports: [
     BrowserModule
